18b5689ed28da5820df7f5ed931bf92052f20b6a
meshjohn <meshjohn1@email.com>
2025-07-28 07:04:37 +0300

    fix some prisma bugs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 import type { NextConfig } from "next";
 //@ts-ignore
 const nextConfig: NextConfig = {
+  devIndicators: false,
+  experimental: {
+    authInterrupts: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
--- next.d.ts
+++ next.d.ts
@@ -3,8 +3,12 @@
 
 type NextPageProps<
   P = Record<string, string | string[] | undefined>, // This is for dynamic routes like /courses/[id]
-  S = Record<string, string | string[] | undefined> // This is for search params like ?search=query
+  S = Record<string, string | string[] | undefined>, // This is for search params like ?search=query
 > = {
   params: Promise<P>; // <-- Make params a Promise
   searchParams?: Promise<S>; // <-- Make searchParams a Promise (if it's not already)
 };
+
+declare module "@prisma/nextjs-monorepo-workaround-plugin" {
+  export class PrismaPlugin {}
+}
